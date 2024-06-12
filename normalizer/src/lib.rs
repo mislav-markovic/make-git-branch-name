7dbc71136c922ef8eb45802ae8d82ce2d7948c6e
Mislav Markovic <mislav.markovic@avl.com>
2024-06-12 16:36:16 +0200

    Add skeleton structure to normalizer and basic unit tests

--- normalizer/src/lib.rs
+++ normalizer/src/lib.rs
@@ -1,5 +1,59 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+pub enum IssueType {
+    Feature,
+    Defect,
+}
+
+pub struct Issue {
+    heading: String,
+    r#type: IssueType,
+}
+
+impl Issue {
+    pub fn new(heading: impl Into<String>, r#type: IssueType) -> Self {
+        let heading: String = heading.into();
+        Self { heading, r#type }
+    }
+}
+
+pub struct ReleaseVersion(String);
+
+impl From<String> for ReleaseVersion {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl From<&str> for ReleaseVersion {
+    fn from(value: &str) -> Self {
+        Self(value.into())
+    }
+}
+
+pub fn normalize_to_path(issue: Issue, for_version: Option<ReleaseVersion>) -> String {
+    let norm_heading = make_normalized_heading(&issue.heading);
+    let type_prefix = make_type_prefix(&issue.r#type);
+    let release_prefix = for_version
+        .map(|r| make_version_prefix(&r))
+        .unwrap_or_default();
+
+    combine_parts(&release_prefix, &type_prefix, &norm_heading)
+}
+
+fn make_normalized_heading(heading: &str) -> String {
+    todo!()
+}
+
+fn make_version_prefix(version: &ReleaseVersion) -> String {
+    todo!()
+}
+
+fn make_type_prefix(r#type: &IssueType) -> String {
+    todo!()
+}
+
+fn combine_parts(release: &str, r#type: &str, issue: &str) -> String {
+    todo!()
 }
 
 #[cfg(test)]
@@ -7,8 +61,69 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn defect_type_correctly_maps() {
+        let defect_type = IssueType::Defect;
+        let norm_type = make_type_prefix(&defect_type);
+
+        assert_eq!("defect".to_string(), norm_type);
+    }
+
+    #[test]
+    fn feature_type_correctly_maps() {
+        let defect_type = IssueType::Feature;
+        let norm_type = make_type_prefix(&defect_type);
+
+        assert_eq!("feature".to_string(), norm_type);
+    }
+
+    #[test]
+    fn release_style_version_string_correctly_made_into_prefix() {
+        let input_version: ReleaseVersion = "R6.4".into();
+        let version_str = make_version_prefix(&input_version);
+
+        assert_eq!("R6/R6.4".to_string(), version_str);
+    }
+
+    #[test]
+    fn semver_style_just_numbers_version_string_correctly_made_into_prefix() {
+        let input_version: ReleaseVersion = "2.4.1".into();
+        let version_str = make_version_prefix(&input_version);
+
+        assert_eq!("2/2.4/2.4.1".to_string(), version_str);
+    }
+
+    #[test]
+    fn semver_style_v_prefix_version_string_correctly_made_into_prefix() {
+        let input_version: ReleaseVersion = "v1.169.420".into();
+        let version_str = make_version_prefix(&input_version);
+
+        assert_eq!("v1/v1.169/v1.169.420".to_string(), version_str);
+    }
+
+    #[test]
+    fn printable_characters_sanitized() {
+        let input = "A:B?C[D\\E^F~G".to_string();
+        let expected = "A_B_C_D_E_F_G".to_string();
+
+        let actual = make_normalized_heading(input.as_str());
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn whitespaces_sanitized() {
+        let input = "A B\tC\r\nD\rE\nF".to_string();
+        let expected = "A_B_C_D_E_F".to_string();
+
+        let actual = make_normalized_heading(input.as_str());
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn multiple_consecutive_replaced_characters_replaced_with_single_character() {
+        let input = "A    B:?[  \\^  ~  C \r\n D\r   E    \n  F".to_string();
+        let expected = "A_B_C_D_E_F".to_string();
+
+        let actual = make_normalized_heading(input.as_str());
+        assert_eq!(expected, actual);
     }
 }
